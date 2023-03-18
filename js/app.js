32d3e0284e1351abe941c7fee36c4850511a5515
developer006tz <developer.ludovic@gmail.com>
2023-03-18 09:53:48 +0300

    add contact form, add sweedish at each part

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         margin: 0,
         items: 1,
         dots: false,
-        navText: ['PREV', 'NEXT'],
+        navText: ['Förra', 'Nästa'],
         smartSpeed: 1000,
         autoplay: true,
         autoplayTimeout: 7000,
